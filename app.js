88622e3e12710d69f281afad055495afdaf6c344
LAMNAZZAH <medlmz01@gmail.com>
2022-05-02 06:00:52 +0000

    student/teacher and buttons

--- app.js
+++ app.js
@@ -34,18 +34,34 @@ app.post("/api/posts", verifyToken, (req, res) => {
 
 //! verifythereistoken && verifyisAdmin && verify
 
-app.get("/api/dashboard",verifyToken, (req, res, next) => {
-    jwt.verify(req.token, "thesecretkey", (err, userAuthData) => {
-        if (err) {
-            res.sendStatus(403); 
-        } else {
-            res.send(`
-              <h1>DASHBOARD</h1>
-              <h2>sensitive information!!</h2>
-              <h3>information: ${userAuthData}</h3>
-              `);
-        }
-    })
+app.get("/api/dashboard", verifyToken, (req, res, next) => {
+  jwt.verify(req.token, "thesecretkey", (err, userAuthData) => {
+    if (err) {
+      res.status(403).send(`
+      <a href='/api/register'>Register</a>
+      <a href='/api/login'>Login</a>
+      `);
+    } else {
+      const data = JSON.stringify(userAuthData);
+
+      if (userAuthData.acctype === "admin") {
+        res.send(`
+            <h1>DASHBOARD</h1>
+            <h2>sensitive information!!</h2>
+            <h3>information: ${data}</h3>
+            <a href='/api/register'>Register</a>
+            <a href='/api/login'>Login</a>
+            `);
+      } else {
+        res.send(`
+            <h1>DASHBOARD</h1>
+            <h2>You are not allowed, upgrade account to teacher please!</h2>
+            <a href='/api/register'>Register</a>
+            <a href='/api/login'>Login</a>
+            `);
+      }
+    }
+  });
 });
 
 app.get("/api/login", (req, res, next) => {
@@ -62,6 +78,8 @@ app.get("/api/login", (req, res, next) => {
     <br/>
     <input type='submit'>
     </form>
+    <a href='/api/register'>Register</a>
+    <a href='/api/dashboard'>dashboard</a>
     `);
 });
 
@@ -75,7 +93,7 @@ app.post("/api/login", async (req, res) => {
   try {
     if (await bcrypt.compare(req.body.password, user.password)) {
       jwt.sign(
-        { user },
+        { name: user.name, acctype: user.acctype },
         "thesecretkey",
         { expiresIn: "180s" },
         (err, token) => {
@@ -107,15 +125,28 @@ app.get("/api/register", (req, res, next) => {
     <input type='password' name='password' placeholder='password' required>
     <br/>
     <br/>
+    <select name='acctype'>
+    <option value='teacher'>teacher</option>
+    <option value='student'>student</option>
+    </select>
+    <br/>
+    <br/>
     <input type='submit'>
-    </form>`);
+    </form>
+    <a href='/api/dashboard'>dashboard</a>
+    <a href='/api/login'>Login</a>
+    `);
 });
 
 app.post("/api/register", async (req, res, next) => {
-  if (req.body.name && req.body.password) {
+  if (req.body.name && req.body.password && req.body.acctype) {
     const salt = await bcrypt.genSalt();
     const hashedPass = await bcrypt.hash(req.body.password, salt);
-    const user = { name: req.body.name, password: hashedPass };
+    const user = {
+      name: req.body.name,
+      password: hashedPass,
+      acctype: req.body.acctype,
+    };
     users.push(user);
     console.log(users);
     res.redirect("/api/login");
@@ -136,7 +167,11 @@ function verifyToken(req, res, next) {
     req.token = bearerToken;
     next();
   } else {
-    res.sendStatus(403);
+    res.status(403).send(`
+        <h2>access token is missing!</h2>
+      <a href='/api/register'>Register</a>
+      <a href='/api/login'>Login</a>
+      `);
   }
 }
 
